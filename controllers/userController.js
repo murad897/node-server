b98ee25cc874289db2b274ab2693ce5cec59a873
user <m.jamburia.websmart@gmail.comgit config --global user.email m.jamburia.websmart@gmail.com>
2022-05-31 12:54:56 +0400

    add login

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,6 +19,24 @@ const registrate_user = async (req, res) => {
   }
 };
 
+const login_user = async (req, res) => {
+  //  Now find the user by their email address
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).send("Incorrect email or password.");
+  }
+
+  // Then validate the Credentials in MongoDB match
+  // those provided in the request
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) {
+    return res.status(400).send("Incorrect email or password.");
+  }
+
+  res.send(true);
+};
+
 module.exports = {
   registrate_user,
+  login_user,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController");
 // register user
 router.post("/register", userController.registrate_user);
 // login user
+router.post("/login", userController.login_user);
 
 module.exports = router;
